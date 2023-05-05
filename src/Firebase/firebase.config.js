5dff66a4f7c6c5d75d9ab1330b8ed278be55fcb3
sajim-khan <shossain1427@gmail.com>
2023-05-05 20:03:18 +0600

    complete project setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLc8gZKZE0cEuJ2g9iWAsntH3y630rMBY",
+  authDomain: "the-dragon-news-52086.firebaseapp.com",
+  projectId: "the-dragon-news-52086",
+  storageBucket: "the-dragon-news-52086.appspot.com",
+  messagingSenderId: "513185322088",
+  appId: "1:513185322088:web:2a6b0f83bd57056800d7dd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Pages/Shared/Navbar/Header';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
